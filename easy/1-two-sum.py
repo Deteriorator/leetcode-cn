c0fb460cf36d2229b20cf86194ba50e6432fdff9
Leon <Liangz.org@gmail.com>
2022-01-01 23:56:15 +0800

    fix: 1-two-sum.py

--- easy/1-two-sum.py
+++ easy/1-two-sum.py
@@ -10,7 +10,6 @@
                    2018/9/25:
 -------------------------------------------------
 """
-__author__ = 'Liangz'
 
 """
 给定一个整数数组和一个目标值，找出数组中和为目标值的两个数。
@@ -24,26 +23,21 @@ __author__ = 'Liangz'
 因为 nums[0] + nums[1] = 2 + 7 = 9
 所以返回 [0, 1]
 """
-nums = []
-class Solution:
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         result = []
         for i in range(0,len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] + nums[j] == target:
-                    result.append(i)
-                    result.append(j)
-
-        return result     # 在LeetCode-cn返回用return，不要print
+            for i in range(0, len(nums)):
+                for j in range(i+1, len(nums)):
+                    if nums[i] + nums[j] == target:
+                        result.append(i)
+                        result.append(j)
+        return result
 
 
 if __name__ == '__main__':
-    nums = [2,7,11,15]
+    nums = [2, 7, 11, 15]
     result = Solution()
     print(result.twoSum(nums, 9))
